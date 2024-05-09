491f036a93dae0e25c83103d71b1f86ae1d63f90
kirin <kirin.mzzt@gmail.com>
2024-05-10 00:37:28 +0300

    - Добавляем в конфиги синхронизацию всех нужных баз данных

--- config/notion.php
+++ config/notion.php
@@ -8,7 +8,30 @@ return [
 
     /** uuid databases */
     'sync_databases' => [
-        '3c36e26a71ff46d984193e7c22309df0', // В долг
-        '844bd11a027d462b8b490df5d807e498', // Общие результаты
+        '3c36e26a71ff46d984193e7c22309df0',
+        '844bd11a027d462b8b490df5d807e498',
+        '8361ff929143448ba07ac163ef7bbb03',
+        '691b7f7d392245f0bc20d340423db25f',
+        'fb8ccb34ab414c538caa2dd12f397fe1',
+        '3f1583ac6dcc41649194a039477484c2',
+        '5e043f42e3c741a39f02fae2d5024e4e',
+        'e5c9e4ec341047aea44becc487ba295b',
+        '7953d7d9a7bd4288b27cdd4e86a41f7a',
+        '6155d3bf43e74cb3ab2b6400ff52335f',
+        'd5bf90e55cd24c8ca3a00bb27f9676e8',
+        '19d6cb28c27a4610ad3fe4e3ad8f6e89',
+        '590b4604f3db46f7893fec3aaab03a83',
+        'be9a6859ebf64debbf63de3bb7e1856f',
+        'bcf4a67f5055446d9a6e84b68d0f1077',
+        '3dce3468b9a44aa79946885c3f526243',
+        '4e6560d7b68f4585bc25d70f09624099',
+        '170c0d0cab674c0da8d900e924bc93a7',
+        'b2bab2654dd342a18bc09a7f58478f25',
+        '15e8ff834bae4227907013748b2eff61',
+        '65d9772500614c3887c51e2ce449973e',
+        '7829503151044e579d75e3ab87527752',
+        '3fe6dcf3ba2948778acbb972a27b5edf',
+        '6afeac04478b411991eedc9d4ab6d896',
+        'ea7bcb28a5ea44a1838423f3cb3bb6b3',
     ],
 ];
